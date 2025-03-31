85f73c169564a2bb05b49799021a51c0565e907c
JamerssonMartins <jamerssondp@gmail.com>
2025-03-31 10:55:16 -0300

    Enviando Ex 04.py e um sistema de rifas

---
+++ Rifas.py
@@ -0,0 +1,7 @@
+print("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-")
+print("Vejamos o número ganhador da Rifa solidária:")
+enter = input("Digite ENTER para sortear um número: ")
+import random
+RIFA = random.randint (1,100)
+print(f"O número ganhador ééééé...{RIFA}")
+print("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-")
